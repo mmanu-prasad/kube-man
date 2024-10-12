bd2b8f2ed871568366cec0df765032f40d502bf5
ManuPrasad <gcpmanuprasad@gmail.com>
2024-10-12 13:32:51 +0530

    added jenkins job dsl

---
+++ linux-pipe.groovy
@@ -0,0 +1,17 @@
+pipelineJob('Hello-Linux') {
+    description('''A sample pipeline job for Hello-Linux project.'''.stripIndent())
+
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url('https://github.com/your-repo/your-project.git')
+                    }
+                    branch('main')
+                }
+            }
+            scriptPath('Jenkinsfile')
+        }
+    }
+}
